bdcb3a273f465b471485b2e1834bcb9f3648e404
Hayat <hayatjahangir0@gmail.com>
2024-11-03 22:44:12 +0530

    link api key and create a function in js file async and wait function use and also convert data  into json api link in fetch

---
+++ weather.js
@@ -0,0 +1,14 @@
+
+const API_KEY = "d1845658f92b31c64bd94f06f7188c9c";
+async function showWeather(){
+
+    // let latitude = 15.3333;
+    // let longitude = 74.0833;
+    let city = 'kashmir';
+
+    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
+
+    const data = await response.json();
+    console.log("weatherdata :->"+data);
+
+}
